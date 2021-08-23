d73256c427e5a05d74231a8186bf3aededb5982b
Victor Shelepen <victor.shelepen@gmail.com>
2021-08-23 18:29:47 +0300

    Page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,  {Fragment } from 'react'
 import { Button } from '@material-ui/core'
+import Header from './Header'
 
 export default function () {
   return (
-    <div>
-      App content
+    <Fragment>
+      <Header />
       <Button color="primary">Button</Button>
-    </div>
+    </Fragment>
   )
 }
