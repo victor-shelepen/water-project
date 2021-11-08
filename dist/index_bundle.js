96455349a87180b655914ce065385f61dd74a644
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-11-08 17:35:43 +0200

    Ignore rules.

--- dist/index_bundle.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";function e(){return React.createElement("div",null,"App content")}ReactDOM.render(React.createElement(e,null),document.getElementById("root"))})();
-//# sourceMappingURL=index_bundle.js.map
