470966560e8108b0aa9f0cb022dd45dd269f3d4e
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-11-08 17:36:36 +0200

    Images work.

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ export default function () {
       <Header />
       <Contacts />
       <Footer />
+      <div class='hero'></div>
     </>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,28 @@ module.exports = {
           // "sass-loader",
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        /**
+         * The `type` setting replaces the need for "url-loader"
+         * and "file-loader" in Webpack 5.
+         *
+         * setting `type` to "asset" will automatically pick between
+         * outputing images to a file, or inlining them in the bundle as base64
+         * with a default max inline size of 8kb
+         */
+        type: "asset",
+
+        /**
+         * If you want to inline larger images, you can set
+         * a custom `maxSize` for inline like so:
+         */
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 30 * 1024,
+        //   },
+        // },
+      },
     ]
   },
   plugins: [
