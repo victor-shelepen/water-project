2140aa93e89f42d8a82c8841ee03517950b0bee6
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-11-10 17:21:35 +0200

    React components have been removed.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import React from 'react'
 
 import '../style/component/App.scss'
 
-import Header from './component/Header'
-import Presentation from './component/Presentation'
-import Promotion from './component/Promotion/component'
-import Contacts from './component/Contacts'
-import Footer from './component/Footer'
-
 export default function () {
   return (
     <>
