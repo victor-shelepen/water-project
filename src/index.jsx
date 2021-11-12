7e8a167a34f21d0d2b5d5cfaba6b6740f771ccb8
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-11-12 19:11:53 +0200

    React has been removed from the project.

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-
 import '../style/index.scss'
 
 document.addEventListener('DOMContentLoaded', function() {
   const appLoadingDOM = document.querySelector('.app-loading');
   appLoadingDOM.remove()
 });
-
-ReactDOM.render(
-  <App />
-  ,
-  document.getElementById('root')
-)
