8a6ecf69981f5b84f1581b92835509c81762f7ac
Victor Shelepen <v.shelepen@services.digifoon.com>
2021-11-20 18:29:59 +0200

    The initial dist subtree commit

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import './on-scroll'
-
-import '../style/index.scss'
-
-document.addEventListener('DOMContentLoaded', function() {
-  const appLoadingDOM = document.querySelector('.app-loading');
-  appLoadingDOM.remove()
-});
